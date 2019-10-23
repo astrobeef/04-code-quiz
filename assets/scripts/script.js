d63f9935746eba8b2e65057ba5e36f1f0d3de103
Brian Graf <brian.g.graf@gmail.com>
2019-10-22 20:42:38 -0700

    Reformatted HTML object and systems

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,10 @@
 //----------HTML REFERENCES----------//
 //-----------------------------------//
-var $topLeft = $("#top-left");
-var $topRight = $("top-right");
 
 //----------INSTANTIATED HTML VARIABLES----------//
 //-----------------------------------------------//
 
-var HS_Link = $("<a>").text("Highscore : X");
-HS_Link.attr("href", "index.html").attr("target", "_blank");
 
-var HS_Cur = $("<h6>").text("Current Score : X");
-
-var HS_Return = $("<a>").text("Return to main");
-HS_Return.attr("href", "index.html").attr("target", "_blank");
 
 //----------VARIABLES----------//
 //-----------------------------//
@@ -20,32 +12,43 @@ HS_Return.attr("href", "index.html").attr("target", "_blank");
 var HTML =
 {
     topLeft: {
-        mDiv: $topLeft,
-        menu: {
-            text: HS_Link
+        mDiv: $("#top-left"),
+        main: {
+            mText: $("<u>").text("Highscores"),
+            mClasses: "p-2",
+            mID : "run-HS"
         },
         testing: {
-            text: HS_Cur
+            mText: $("<span>").text("Current Score : "),
+            mClasses: "p-2",
+            mID : "cur-score"
         },
         finished: {
-            text: HS_Link
+            mText: $("<u>").text("Highscores"),
+            mClasses: "p-2",
+            mID : "run-HS"
         },
         highscores:
         {
-            text: HS_Return
+            mText: $("<u>").text("Return to main"),
+            mClasses : "p-2",
+            mID : "run-main"
         }
+        
     },
 
     topRight: {
-        mDiv: $topRight,
-        menu: {
-
+        mDiv: $("#top-right"),
+        main: {
+            mText : $("<h6>").text("A")
         },
         testing: {
-
+            mText : $("<h6>").text("Time left : "),
+            mID : "timer"
         },
         finished: {
-
+            mText : $("<h6>").text("Time Left over : "),
+            mID : "timer"
         }
     }
 };
@@ -53,38 +56,67 @@ var HTML =
 //----------FUNCTIONS----------//
 //-----------------------------//
 
-//Set up the elements of the 'menu' display.
-function SetMenuElements() {console.log("Setting up menu elements...");
+//Set up the elements of the 'main' display.
+function SetMainElements() {
+    console.log("Setting up main elements...");
     HTML.topLeft.mDiv.empty();
-    HTML.topLeft.mDiv.append(HTML.topLeft.menu.text);
+    HTML.topRight.mDiv.empty();
+
+
+    $iTopLeft = ModifyText(HTML.topLeft.main);
+    HTML.topLeft.mDiv.append($iTopLeft);
+
+    $iTopRight = ModifyText(HTML.topRight.main);
+    HTML.topRight.mDiv.append($iTopRight);
 }
 
 //Sets up the elements of the 'testing' display.
-function SetTestingElements() {console.log("Setting up testing elements...");
+function SetTestingElements() {
+    console.log("Setting up testing elements...");
     HTML.topLeft.mDiv.empty();
-    HTML.topLeft.mDiv.append(HTML.topLeft.testing.text);
+
+    $iTopLeft = ModifyText(HTML.topLeft.testing);
+    HTML.topLeft.mDiv.append($iTopLeft);
+
+    $iTopRight = ModifyText(HTML.topRight.testing);
+    HTML.topRight.mDiv.append($iTopRight);
 }
 
 //Sets up the elements of the 'finished' display.
-function SetFinishedElements() {console.log("Setting up Finished elements...");
+function SetFinishedElements() {
+    console.log("Setting up Finished elements...");
     HTML.topLeft.mDiv.empty();
-    HTML.topLeft.mDiv.append(HTML.topLeft.finished.text);
 }
 
 //Sets up the elements of the 'Highscores' display.
-function SetHSElements() {console.log("Setting up Highscore elements...");
+function SetHSElements() {
+    console.log("Setting up Highscore elements...");
     HTML.topLeft.mDiv.empty();
-    HTML.topLeft.mDiv.append(HTML.topLeft.highscores.text);    
+
+    var $iTopLeft = ModifyText(HTML.topLeft.highscores);
+    HTML.topLeft.mDiv.append($iTopLeft);
+
+    var $iTopRight = ModifyText(HTML.topRight.highscores);
+    HTML.topRight.mDiv.append($iTopRight);
+}
+
+//Sets up the text to be appended to its Div.
+function ModifyText(pPrefix)
+{
+    var $iDiv = pPrefix.mText;
+    $iDiv.attr("id", pPrefix.mID);
+    $iDiv.attr("class", pPrefix.mClasses);
+
+    return $iDiv;
 }
 
 //----------MAIN----------//
 //------------------------//
 
 //A prompt for testing which triggers the previous functions.
-switch(prompt("Input run 0-3"))
-{
+switch (prompt("Input run 0-3")) {
     case "0":
-        SetMenuElements();
+        SetMainElements();
         break;
     case "1":
         SetTestingElements();
@@ -96,3 +128,27 @@ switch(prompt("Input run 0-3"))
         SetHSElements();
         break;
 }
+
+//----------EVENTS----------//
+//--------------------------//
+
+
+//Adds a click event to the HTML element identified as '#btn-start'
+//This event will begin the function 'SetTestingElements'
+$("#btn-start").on("click", function (e) {
+    SetTestingElements();
+})
+
+$("#top-left").on("click", function (e) {
+    var $target = $(event.target);
+    var $target_ID = $(event.target).attr("id");
+
+    switch ($target_ID) {
+        case "run-main":
+            SetMainElements();
+            break;
+        case "run-HS":
+            SetHSElements();
+            break;
+    }
+})
